c14f3426d287231b4634414c138ff8aca3a2d2e6
jalaj <jaggyjalaj@gmail.com>
2024-12-30 20:42:16 +0530

    some more more updates

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,15 @@ push.setVapidDetails(
   vapIdKeys.privateKey
 );
 
-let sub: any = {};
+let sub: any = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/fSyvomAI7EE:APA91bGYmNQN9TeMzxgTjPJQEjPewQq1O_z92e0PKes3rkEqKEv5YN7J14fUKM5GMfbPANdSUVsB9osxYYHKo3EGNuCLZsa7GovBofP41713ffnWRWpvQkDRKZeQTPOYfb0O5zoDDOnn",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BAGFLSI6P6Z9bbpCY6mK1kRFxzhhjhrQe-qTz85yv5Mxnbwr2v6OPPPXIjY4agiJU_TeEFvfln-d8WVSTubTwMs",
+    auth: "s-150pcAO2VwIXd9I7nyfA",
+  },
+};
 
 push.sendNotification(sub, "test message");
